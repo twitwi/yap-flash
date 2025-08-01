7afa137d65322c4d526cd380a344bbc251226471
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-08-01 23:38:40 +0200

    Naive UI Integration (+45kB, 71->116)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import ReloadPrompt from './components/ReloadPrompt.vue'
+import { NButton, NButtonGroup, NConfigProvider } from 'naive-ui'
+import { bindRouterLink, getTheme } from './main-naiveui'
+
+const color = import.meta.env.VITE_THEME_HEXCOLOR
+const theme = getTheme(color)
+
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/lib">Library</RouterLink>
-    </nav>
-  </header>
+  <NConfigProvider :theme-overrides="theme">
+    <header>
+      <nav>
+        <NButtonGroup class="header">
+          <NButton v-bind="bindRouterLink('home')">Home</NButton>
+          <NButton v-bind="bindRouterLink('library')">Library</NButton>
+          <NButton v-bind="bindRouterLink('config')" style="flex: 0">cfg</NButton>
+        </NButtonGroup>
+      </nav>
+    </header>
 
-  <RouterView />
+    <RouterView />
+  </NConfigProvider>
   <ReloadPrompt />
 </template>
+
+<style>
+.header {
+  width: 100%;
+  display: flex;
+  & > * {
+    flex: 1;
+  }
+}
+</style>
