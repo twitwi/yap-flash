cb984967bec64aff56c5344ba90b08827491c9fa
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-08-05 22:57:09 +0200

    Prevent matching stuffimg:// etc

--- src/components/MDRender.vue
+++ src/components/MDRender.vue
@@ -10,14 +10,14 @@ const props = defineProps<{ content: string }>()
 const md = new markdownit('commonmark', {})
 const htmlContent = ref('')
 function updateContent() {
-  const content = props.content.replace(/img:\/\/([a-z0-9]+)/g, (match, p1) => {
+  const content = props.content.replace(/(?<!\w)img:\/\/([a-z0-9]+)/g, (match, p1) => {
     const bblob = main.binaries[p1]
     if (bblob) {
       return `<img src="${getPersistentObjectURL(bblob)}" />`
     } else {
       return `(((missing binary ${p1})))`
     }
-  }).replace(/bin:\/\/([a-z0-9]+)/g, (match, p1) => {
+  }).replace(/(?<!\w)bin:\/\/([a-z0-9]+)/g, (match, p1) => {
     const bblob = main.binaries[p1]
     if (bblob) {
       return getPersistentObjectURL(bblob)
