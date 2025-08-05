3c3602c6c0ac4b17eec4c1ff30e17f1ba2284241
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-08-05 23:04:27 +0200

    Allow dashes in bin id (in case we want non random, readable ones)

--- src/components/MDRender.vue
+++ src/components/MDRender.vue
@@ -10,14 +10,14 @@ const props = defineProps<{ content: string }>()
 const md = new markdownit('commonmark', {})
 const htmlContent = ref('')
 function updateContent() {
-  const content = props.content.replace(/(?<!\w)img:\/\/([a-z0-9]+)(([.][^.\s]+)*)/g, (match, p1, p2) => {
+  const content = props.content.replace(/(?<!\w)img:\/\/([-a-z0-9]+)(([.][^.\s]+)*)/g, (match, p1, p2) => {
     const bblob = main.binaries[p1]
     if (bblob) {
       return `<img src="${getPersistentObjectURL(bblob)}" class="${p2 ? p2.replace(/[.]/g, ' ').trim() : ''}" />`
     } else {
       return `(((missing binary ${p1})))`
     }
-  }).replace(/(?<!\w)bin:\/\/([a-z0-9]+)/g, (match, p1) => {
+  }).replace(/(?<!\w)bin:\/\/([-a-z0-9]+)/g, (match, p1) => {
     const bblob = main.binaries[p1]
     if (bblob) {
       return getPersistentObjectURL(bblob)
