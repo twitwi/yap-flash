2ab97e91154368612c1d451daabc2cbe24a49e0c
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-08-09 00:31:35 +0200

    A form of algo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,101 @@
 <script setup lang="ts">
 import MDRender from '@/components/MDRender.vue'
 import { useLocalStore, useMainStore } from '@/stores/simple'
-import { Outcomes, type Outcome } from '@/typing'
-import { NButton, NButtonGroup, NSpace } from 'naive-ui'
+import { Outcomes, type Outcome, type CardId, type Card } from '@/typing'
+import { NButton, NSpace } from 'naive-ui'
 import { computed, ref } from 'vue'
 
 const local = useLocalStore()
 const main = useMainStore()
 
+// could move to the store
+const dueDates = computed(() => {
+  type Hist = {
+    lastTime: number
+    lastOutcome: Outcome
+    okStreak: number
+    due: number
+  }
+  const HOUR = 1000 * 3600
+  const DAY = HOUR * 24
+  const hrandom = () => (Math.random() - 0.5) * HOUR
+  const res = new Map<CardId, Hist>()
+  for (const v of main.views) {
+    if (v.outcome === 'skip' || v.outcome === 'stop') {
+      // TODO probably do something with skip
+      // TODO consider stop as overtrivial
+      continue
+    }
+    if (!res.has(v.cardId)) {
+      res.set(v.cardId, { lastTime: 0, lastOutcome: 'wrong', okStreak: 0, due: 0 })
+    }
+    const hist = res.get(v.cardId)!
+    if (['trivial', 'ok'].includes(v.outcome)) {
+      hist.due = v.when + Math.pow(2, hist.okStreak) * (DAY + hrandom())
+      hist.okStreak += 1
+      if (v.outcome === 'trivial') {
+        hist.okStreak += .25
+      }
+    } else {
+      hist.okStreak = 0
+      hist.due = v.when + Math.abs(hrandom()) / 60
+    }
+    hist.due = Math.round(hist.due)
+    hist.lastTime = v.when
+    hist.lastOutcome = v.outcome
+  }
+  for (const c of main.cards) {
+    if (!res.has(c.id)) {
+      res.set(c.id, { lastTime: 0, lastOutcome: 'wrong', okStreak: 0, due: hrandom() })
+    }
+  }
+  return res
+})
+
+function minBy<T>(arr: Iterable<T>, f: (v: T) => number) {
+  let minT = null
+  let minF = null
+  for (const v of arr) {
+    if (minF === null) {
+      minT = v
+      minF = f(v)
+    } else {
+      const vf = f(v)
+      if (vf < minF) {
+        minT = v
+        minF = vf
+      }
+    }
+  }
+  return minT as T
+}
+
+const DUMMY_CARD = {
+  id: 'dummy',
+  front: '',
+  back: '',
+  tags: [],
+  type: 'basic',
+} as Card
+
 const next = computed(() => {
+  if (dueDates.value.size == 0) {
+    return { card: DUMMY_CARD }
+  }
+  const [id, hist] = minBy(dueDates.value.entries(), (e) => e[1].due)
+
+  const card = main.cards.find(c => c.id === id)
+  if (card === undefined) {
+    return { card: DUMMY_CARD }
+  }
+  return { card, upToDate: hist.due >= Date.now() }
+
+  /*
   const i = Math.floor(Math.random() * main.cards.length)
   return {
     card: main.cards[i],
     upToDate: false, // whether there is no need to rehearse anymore
-  }
+  }*/
 })
 
 function registerFeedback(outcome: Outcome) {
@@ -41,6 +123,7 @@ function customText(outcome: Outcome): string {
 </script>
 
 <template>
+  <div v-if="next.upToDate" style="text-align: center;">You're up to date, you can still practice if you want.</div>
   <div class="card-front" v-if="next.card && state === 'idle'">
     <NSpace style="width: 100% ;" justify="space-between">
       <NButton @click="state = 'flipped'" type="primary">Flip</NButton>
